8effa9559f2163e63b8ad9dce62424e0140fbfc3
Jane Marimo <jenny.bulatova@gmail.com>
2023-11-11 16:03:19 +0300

    Ура с 50ого раза вышло

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+// TODO: write your code here
+import sum from './basic';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
+
+export default function getHealthStatus(data) {
+  if (data.health >= 50) {
+    return 'healthy';
+  }
+
+  if (data.health < 50 && data.health >= 15) {
+    return 'wounded';
+  }
+
+  if (data.health < 15 && data.health >= 1) {
+    return 'critical';
+  }
+
+  if (data.health === 0 || data.health < 0) {
+    return 'dead';
+  }
+
+  return 'dead';
+};
+
+const heroesInfo = [
+  {name: 'мечник', health: 10},
+  {name: 'маг', health: 100},
+  {name: 'лучник', health: 80},
+];
+
+export const sortedHeroes = heroesInfo.sort(function(x, y) {
+  return y.health - x.health;
+});
